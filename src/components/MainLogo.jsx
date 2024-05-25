a09d13375d1d16fbd0c0eea0eabc1ab282c1d358
MahakGarg1 <139383370+MahakGarg1@users.noreply.github.com>
2024-05-25 22:29:01 +0530

    Update MainLogo.jsx

--- src/components/MainLogo.jsx
+++ src/components/MainLogo.jsx
@@ -5,7 +5,7 @@ const MainLogo = ({ imgSrc }) => {
   return (
     <div className="main-logo">
       {imgSrc && <img src={imgSrc} alt="Main Logo" className="main-logo-img" />}
-      <h1>Welcome to E-Cart</h1>
+   
     </div>
   );
 };
